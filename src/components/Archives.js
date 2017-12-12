bd0f5e2d280a2518cf0b59b136f3993f94daa632
bexaca <aleksandarberonja6@gmail.com>
2017-12-12 15:26:26 +0100

    fetch config

--- src/components/Archives.js
+++ src/components/Archives.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 export class Archives extends React.Component {
+    componentDidMount() {
+        const url = "https://api.football-data.org/v1/fixtures";
+        const token = "3edb1bdd0041436ebc77c561b73e5e07";
+        fetch(
+            url, 
+            {
+                headers: { 'X-Auth-Token': token }
+            }) 
+            .then(d => d.json())
+            .then(d => {
+            for(let i = 0; i < d.count; i++){
+                console.log(d.fixtures[i])
+            }
+        })
+        .catch(function(error) {
+            console.log('You failed me', error);
+        });
+        
+    }
     render() {
         return (
             <div className="container">
