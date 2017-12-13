a0a3e405aa87b0928fcead3f73d1e23036bceeb4
bexaca <33089360+bexaca@users.noreply.github.com>
2017-12-13 23:26:42 +0100

    super agent

--- src/components/Archives.js
+++ src/components/Archives.js
@@ -1,39 +1,53 @@
 import React from 'react';
+import request from 'superagent';
 
 export class Archives extends React.Component {
-    componentDidMount() {
-        const url = "http://api.football-data.org/v1/teams/57/players";
-        const token = "3edb1bdd0041436ebc77c561b73e5e07";
-        fetch(
-            url, 
-            {
-                headers: { 'X-Auth-Token': token }
-            }) 
-            .then(d => d.json())
-            .then(d => {
-            for(let i=0; i<d.count; i++){
-                if(d.players[i].position === 'Keeper'){
-                    console.log(d.players[i].name)
-                }
-            }
-            for(let i=0; i<d.count; i++){
-                if(d.players[i].position === 'Centre-Forward'){
-                    console.log(d.players[i].name)
-                }
-            }
-            
-        })
-        .catch(function(error) {
-            console.log('You failed me', error);
-        });
-        
+    constructor(props) {
+        super(props);
+        this.state = {
+            response: null
+        }
     }
+
+
+    componentWillMount() {
+        const url = "http://api.football-data.org/v1/teams/62/players";
+        const token = "3edb1bdd0041436ebc77c561b73e5e07";
+
+        request
+            .get(url)
+            .set('X-Auth-Token', token)
+            .set('accept', 'json')
+            .end((err, res) => {
+                if (err || !res.ok) {
+                    alert('Oh no! error');
+                } else {
+                    const responses = res.body;
+                    this.setState({
+                        response: responses
+                    })
+                }
+            });
+    }
+
+
     render() {
+        const response = this.state.response;
+
+        if (response != null) {
+            console.log(this.state.response)
+            return (
+                <div className="container">
+                    <h1 className="text-center">{response.players[2].name}</h1>
+                </div>
+            );
+        }
         return (
             <div className="container">
-                <h1 className="text-center">Test Archives</h1>
+                <h1 className="text-center"></h1>
             </div>
         );
+
     }
 }
 
